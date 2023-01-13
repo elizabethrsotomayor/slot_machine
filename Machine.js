ad42cdcf920a5f7ae3d464e4429393e649a5043d
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2023-01-13 15:55:01 -0600

    first commit

---
+++ Machine.js
@@ -0,0 +1,16 @@
+class Machine extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          {this.props.s1} {this.props.s2} {this.props.s3}
+        </p>
+        <p>
+          {this.props.s1 === this.props.s2 && this.props.s2 === this.props.s3
+            ? "You win!"
+            : "You lose!"}
+        </p>
+      </div>
+    );
+  }
+}
